91fab824dcf35241b7fc838cf7eccb74e142a715
Harsh <harshsharma102000@gmail.com>
2020-08-20 10:57:55 +0530

    SearchingandSortingStart

--- Spiral.cpp
+++ Spiral.cpp
@@ -1,22 +1,56 @@
+
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int rows, column;
-    cout << "Enter no of rows and column\n";
-    cin >> rows >> column;
-    int a[rows][column];
-    cout<<"Enter elements\n";
-    for (int i = 0; i < rows; i++)
+    int i, k, j, A, str, stc, enr, enc, dif;
+    cout << "Enter the number: \n";
+    cin >> A;
+    if (A < 1 || A > 1000)
     {
-        for (int j = 0; j < rows; j++)
+        exit(0);
+    }
+    else
+    {
+        int a[A][A];
+        str = stc = dif = 0;
+        enr = A - 1;
+        enc = A - 1;
+        k = 1;
+        cout << "\nThe matrix is: \n";
+        while (str <= enr && stc <= enc)
         {
-            cin>>a[i][j];
+
+            for (i = stc; i <= enc; i++)
+            {
+                a[str][i] = k;
+                k++;
+            }
+            str++;
+            for (i = str; i <= enr; i++)
+            {
+                a[i][enc] = k;
+                k++;
+            }
+            enc--;
+            for (i = enc; i >= stc; i--)
+            {
+                a[enr][i] = k;
+                k++;
+            }
+            enr--;
+            for (i = enr; i >= str; i--)
+            {
+                a[i][stc] = k;
+                k++;
+            }
+            stc++;
         }
     }
-
-
-
-    return 0;
+    for (i = 0; i < A; i++)
+    {
+        for (j = 0; j < A; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
 }
