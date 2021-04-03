0bdc0c82dc276a915a6e0f0f3857136dedaa05fa
Harsh <harshsharma102000@gmail.com>
2021-04-03 15:25:48 +0530

    New Items

--- class.cpp
+++ class.cpp
@@ -1,66 +1,72 @@
-// Binary Search Tree - Implemenation in C++
-// Simple program to create a BST of integers and search an element in it 
-#include<iostream>
-using namespace std;
-//Definition of Node for Binary search tree
-struct BstNode {
-	int data; 
-	BstNode* left;
-	BstNode* right;
+#include <stdio.h>
+#include <string.h>
+struct student
+{
+	char stu_name[30];
+	int roll;
+	char group;
+	float fees;
 };
+struct student stu;
+void set_data()
+{
+	printf("Enter details of Student:  \n name \nroll \nfees\n  ");
+	scanf("%s", &stu.stu_name);
+	scanf("%d", &stu.roll);
+	scanf("%f", &stu.fees);
+}
+void print_data()
+{
+	printf("Details are :\n");
+	printf("%s \n %d \n %f ", stu.stu_name, stu.roll, stu.fees);
+}
+void allocate_group()
+{
+	if ((stu.stu_name[0] >= 'A' && stu.stu_name[0] <= 'P') || (stu.stu_name[0] >= 'a' && stu.stu_name[0] <= 'p'))
+		stu.group = 'A';
+	else
+		stu.group = 'B';
+}
+void alternative_charater()
+{
+	int i;
+	for (i = 0; i < strlen(stu.stu_name); i = i + 2)
+	{
+		printf("%c", stu.stu_name[i]);
+	}
+}
+int main()
+{
+	char choice;
+	int ch;
+	struct stu;
 
-// Function to create a new Node in heap
-BstNode* GetNewNode(int data) {
-	BstNode* newNode = new BstNode();
-	newNode->data = data;
-	newNode->left = newNode->right = NULL;
-	return newNode;
-}
-
-// To insert data in BST, returns address of root node 
-BstNode* Insert(BstNode* root,int data) {
-	if(root == NULL) { // empty tree
-		root = GetNewNode(data);
-	}
-	// if data to be inserted is lesser, insert in left subtree. 
-	else if(data <= root->data) {
-		root->left = Insert(root->left,data);
-	}
-	// else, insert in right subtree. 
-	else {
-		root->right = Insert(root->right,data);
-	}
-	return root;
-}
-//To search an element in BST, returns true if element is found
-bool Search(BstNode* root,int data) {
-	if(root == NULL) {
-		return false;
-	}
-	else if(root->data == data) {
-		return true;
-	}
-	else if(data <= root->data) {
-		return Search(root->left,data);
-	}
-	else {
-		return Search(root->right,data);
-	}
-}
-int main() {
-	BstNode* root = NULL;  // Creating an empty tree
-	/*Code to test the logic*/
-	root = Insert(root,15);	
-	root = Insert(root,10);	
-	root = Insert(root,20);
-	root = Insert(root,25);
-	root = Insert(root,8);
-	root = Insert(root,12);
-	// Ask user to enter a number.  
-	int number;
-	cout<<"Enter number be searched\n";
-	cin>>number;
-	//If number is found, print "FOUND"
-	if(Search(root,number) == true) cout<<"Found\n";
-	else cout<<"Not Found\n";
+	do
+	{
+		printf("\n Press 1 ENTER DETAILS OF STUDENT EXCEPT GROUP ");
+		printf("\n Press 2 TO PRINT THE DETAILS OF STUDENT EXCEPT GROUP ");
+		printf("\n Press 3 TO ALLOCATE GROUP A OR B");
+		printf("\n Press 4 PRINTING ALTERENATIVE CHARACTER TO STUDENT NAME \n");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+		case 1:
+			set_data();
+			break;
+		case 2:
+			print_data();
+			break;
+		case 3:
+			allocate_group();
+			break;
+		case 4:
+			alternative_charater();
+			break;
+		default:
+			printf("Wrong choice!");
+		}
+		printf("\n Do you want to continue? (Press y/n)");
+		scanf(" %c", &choice);
+	} while (choice == 'y');
+	return 0;
 }
