2dc36b2bf4031d5bcd03404228290154dfbb2d14
Harsh <harshsharma102000@gmail.com>
2021-04-24 10:16:01 +0530

    Imp ques added

--- class.cpp
+++ class.cpp
@@ -1,93 +1,26 @@
 #include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-
-/*
- * Complete the 'getMax' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts STRING_ARRAY operations as parameter.
- */
-
-vector<int> getMax(vector<string> operations) {
-    vector <int> v;
-    string a="";
-    for(int i=0;i<operations.size();i++)
-    {
-        string a=operations[i];
-        if(a[0]=="1")
-        {
-            v.push_back((stoi)(a.substr(2,a.length()-1)));
-            cout<<"1st"<<endl;
-        }   
-        if(a[0]=="2")
-        {
-            cout<<"2nd"<<endl;
-        }   
-        if(a[0]=="3")
-        {
-            cout<<"3rd"<<endl;
-        }        
-    }
-    return v;
-}
-
-int main()
+#include <vector>
+class Solution
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> a;
 
-    string n_temp;
-    getline(cin, n_temp);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int a = nums[i];
+            for (int j = i + 1; j < nums.size(); j++)
+            {
 
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    vector<string> ops(n);
-
-    for (int i = 0; i < n; i++) {
-        string ops_item;
-        getline(cin, ops_item);
-
-        ops[i] = ops_item;
-    }
-
-    vector<int> res = getMax(ops);
-
-    for (size_t i = 0; i < res.size(); i++) {
-        fout << res[i];
-
-        if (i != res.size() - 1) {
-            fout << "\n";
+                if (nums[j] * a == target)
+                {
+                    a.push_back(i);
+                    a.push_back(j);
+                    break;
+                }
+            }
         }
+        return a;
     }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
+};
