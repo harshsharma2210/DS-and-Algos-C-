fa0952c2f02f4f46fb32d17f6a63503b12ac0366
Harsh <harshsharma102000@gmail.com>
2021-07-06 20:46:06 +0530

    Updated

--- STL/MaxCircles.cpp
+++ STL/MaxCircles.cpp
@@ -15,3 +15,37 @@
 // Output Format: Print a single integer denoting the answer.
 // Sample Output: 2
 
+#include <bits/stdc++.h>
+using namespace std;
+bool compare(const pair<int, int> a, const pair<int, int> b)
+{
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        v.push_back(make_pair(a - b, a + b));
+    }
+    sort(v.begin(), v.end(), compare);
+    int count = 0;
+    int end = v[0].second;
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i].first < end)
+        {
+            end = v[i - 1].second;
+            continue;
+        }
+        count++;
+    }
+    cout << count;
+}
