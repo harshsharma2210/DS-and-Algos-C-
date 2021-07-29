b18526bad90774edf54f1461f53c5647bae4e548
Harsh <harshsharma102000@gmail.com>
2021-07-29 10:12:57 +0530

    NEW

--- Random.cpp
+++ Random.cpp
@@ -1,42 +1,114 @@
-// C++ program for the above approach
+// { Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    string findOrder(string dict[], int N, int K)
+    {
+        //code here
+
+        vector<int> g[K];
+        int indeg[K] = {0};
+        for (int i = 0; i < N - 1; i++)
+        {
+            string w1 = dict[i];
+            string w2 = dict[i + 1];
+            for (int j = 0; j < min(w1.length(), w2.length()); j++)
+            {
+                if (w1[j] != w2[j])
+                {
+                    g[w1[j] - 'a'].push_back(w2[j] - 'a');
+                    indeg[w2[j] - 'a']++;
+                }
+            }
+        }
+        queue<string> q;
+        for (int i = 0; i < K; i++)
+        {
+            for (auto x : g[i])
+            {
+                if (indeg[x] == 0)
+                    q.push(to_string(x + 'a'));
+            }
+        }
+        string s = "";
+        while (!q.empty())
+        {
+            int top = stoi(q.front());
+            q.pop();
+            s += to_string(top);
+            cout << s << " ";
+            for (auto x : g[top])
+            {
+                indeg[x]--;
+                if (indeg[x] == 0)
+                    q.push(to_string(x + 'a'));
+            }
+        }
+        return s;
+    }
+};
+
+// { Driver Code Starts.
+string order;
+bool f(string a, string b)
+{
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++)
+    {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size())
+        return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
 int main()
 {
-    int V;
-    cin >> V;
-    int indeg[V + 1] = {0};
-    vector<int> adj[V + 1];
-    for (int i = 0; i < V; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        int x, y;
-        cin >> x >> y;
-        adj[x].push_back(y);
-        indeg[y]++;
-    }
-    queue<int> q;
-    for (int i = 0; i < V; i++)
-    {
-        for (auto x : adj[i])
-            if (indeg[x] == 0)
-            {
-                q.push(x);
-                ans[i] = 1;
-            }
-    }
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++)
+            cin >> dict[i];
 
-    int c = 0;
-    vector<int> ans;
-    while (!q.empty())
-    {
-        int top = q.front();
-        q.pop();
-        c = q.size();
-        for (auto x : adj[top])
-        {
-        }
-    }
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++)
+            order += ans[i];
 
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i])
+                f = false;
+
+        if (f)
+            cout << 1;
+        else
+            cout << 0;
+        cout << endl;
+    }
     return 0;
 }
+// } Driver Code Ends
