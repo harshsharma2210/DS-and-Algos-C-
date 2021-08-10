367fa03011c48fbd653dd195991ffb3339edf8fc
Harsh <harshsharma102000@gmail.com>
2021-08-10 17:06:41 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,32 +1,13 @@
-class Solution
+int AlternatingaMaxLength(vector<int> &nums)
 {
-public:
-    int dp[51][51];
-    int printMaxSubSquare(vector<vector<int>> &M, int m, int n, int *maxv)
+    int inc = 1;
+    int dec = 1;
+    for (int i = 1; i < nums.size(); i++)
     {
-        int res;
-        if (m < 0 || n < 0)
-            return 0;
-        if (dp[m][n] != -1)
-            return dp[m][n];
-        if (M[m][n] == 1)
-        {
-            res = 1 + min(printMaxSubSquare(M, m, n - 1, maxv), min(printMaxSubSquare(M, m - 1, n, maxv), printMaxSubSquare(M, m - 1, n - 1, maxv)));
-        }
-        else
-        {
-
-            res = 0;
-        }
-        *maxv = max(*maxv, res);
-        return dp[m][n] = res;
+        if (nums[i - 1] < nums[i])
+            inc = dec + 1;
+        else if (nums[i - 1] > nums[i])
+            dec = inc + 1;
     }
-    int maxSquare(int n, int m, vector<vector<int>> mat)
-    {
-        // code here
-        int maxv = 0;
-        memset(dp, -1, sizeof(dp));
-        printMaxSubSquare(mat, n - 1, m - 1, &maxv);
-        return maxv;
-    }
-};
+    return max(inc, dec);
+}
