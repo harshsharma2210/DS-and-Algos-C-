a9573c9f496dd105ce69eea61d6d3f8846e38ef9
Harsh <harshsharma102000@gmail.com>
2021-08-29 17:09:47 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,51 +1,17 @@
-// { Driver Code Starts
-//Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution
-{
-public:
-    string solve(int arr[], int n)
+int jumps = 0, farthest = 0, currEnd = 0;
+    for(int i=0;i<n-1;i++)
     {
-        // code here
-        string ans1, ans2;
-        ans1 = ans2 = "";
-        sort(arr, arr + n);
-        for (int i = 0; i < n; i += 2)
+        farthest = max(farthest, i + arr[i]);
+        if(farthest >= n-1)
+            return 1 + jumps;
+        
+        if(i == farthest)
+            return -1;
+        
+        if(i == currEnd)
         {
-            if (i & 2 == 0)
-                ans1 = ans1 + to_string(arr[i]);
-            else
-                ans2 = ans2 + to_string(arr[i + 1]);
+            jumps++;
+            currEnd = farthest;
         }
-        ans1 = to_string(stoi(ans1) + stoi(ans2));
-
-        return (ans1);
     }
-};
-
-// { Driver Code Starts.
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        Solution ob;
-        auto ans = ob.solve(arr, n);
-        cout << ans << "\n";
-    }
-    return 0;
-} // } Driver Code Ends
+    return jumps;
