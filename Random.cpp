8ca4846cbd4b26bd085f1f15e90f85508a899730
Harsh <harshsharma102000@gmail.com>
2021-08-30 09:34:24 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,17 +1,53 @@
-int jumps = 0, farthest = 0, currEnd = 0;
-    for(int i=0;i<n-1;i++)
+class Solution
+{
+public:
+    vector<int> findAnagrams(string s, string p)
     {
-        farthest = max(farthest, i + arr[i]);
-        if(farthest >= n-1)
-            return 1 + jumps;
-        
-        if(i == farthest)
-            return -1;
-        
-        if(i == currEnd)
+        int i = 0, j = 0, counter, tn = p.size(), n = s.size();
+        vector<int> l;
+        unordered_map<char, int> tm;
+        unordered_map<char, int> m;
+        //intitalise the target map first for further references
+        for (int k = 0; k < p.size(); k++)
         {
-            jumps++;
-            currEnd = farthest;
+            tm[p[k]]++;
+            m[p[k]]++;
         }
+        counter = tm.size();
+        while (j < n)
+        {
+            if (tm[s[j]] > 0)
+            {
+                m[s[j]]--;
+                if (m[s[j]] == 0)
+                {
+                    counter--;
+                }
+            }
+
+            if ((j - i + 1) < tn)
+            {
+                j++;
+            }
+            else if ((j - i + 1) == tn)
+            {
+                if (counter == 0)
+                {
+                    l.push_back(i);
+                }
+                if (tm[s[i]] > 0)
+                {
+                    m[s[i]]++;
+                    if (m[s[i]] == 1)
+                    {
+                        counter++;
+                    }
+                }
+
+                i++;
+                j++;
+            }
+        }
+        return l;
     }
-    return jumps;
+};
