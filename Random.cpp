2ad5f7fea120ecd1ca16fe70ad81cf11a9649854
Harsh <harshsharma102000@gmail.com>
2021-09-01 12:17:15 +0530

    Nwq

--- Random.cpp
+++ Random.cpp
@@ -1,27 +1,138 @@
-int sb(int arr[], int n, int x)
+// { Driver Code Starts
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
 {
-    // Your code goes here
-    int i = 0;
-    int j = 0;
-    int currsum = 0;
-    int ans = INT_MAX;
-    while (j < n)
+    int data;
+    Node *next;
+
+    Node(int val)
     {
-        = "" currsum += "arr[j];" if (currsum < x) = ""
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node *head, Node *tail, int position)
+{
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node *head)
+{
+    if (!head)
+        return false;
+
+    Node *fast = head->next;
+    Node *slow = head;
+
+    while (fast != slow)
+    {
+        if (!fast || !fast->next)
+            return false;
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
+
+int length(Node *head)
+{
+    int ret = 0;
+    while (head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+// } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node *head)
+    {
+        // code here
+        // just remove the loop without losing any nodes
+        if (!head)
+            return;
+        unordered_map<Node *, int> a;
+        while (head != NULL)
         {
-            = "" j++;
-            = ""
-        }
-        = "" else = "" if (currsum = "" >= x)
-        {
-            while (currsum > x)
+            if (a.find(head) != a.end())
             {
-                ans = min(j - i + 1, ans);
-                currsum -= arr[i];
-                i++;
+                head->next = NULL;
             }
-            j++;
+
+            a[head] = 1;
+            head = head->next;
         }
     }
-    return ans;
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, num;
+        cin >> n;
+
+        Node *head, *tail;
+        cin >> num;
+        head = tail = new Node(num);
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+
+        int pos;
+        cin >> pos;
+        loopHere(head, tail, pos);
+
+        Solution ob;
+        ob.removeLoop(head);
+
+        if (isLoop(head) || length(head) != n)
+            cout << "0\n";
+        else
+            cout << "1\n";
+    }
+    return 0;
 }
+// } Driver Code Ends
