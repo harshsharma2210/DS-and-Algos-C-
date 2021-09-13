295f8209428338ebdca8cf0791274ad346cd06f7
Harsh <harshsharma102000@gmail.com>
2021-09-13 10:38:35 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,14 +1,22 @@
-vector<int> temp;
-for (auto i : v)
-    temp.insert(temp.end(), i.begin(), i.end());
-sort(temp.begin(), temp.end());
-
-int counter = 0;
-for (int i = 0; i < v.size(); i++)
-{
-    for (int j = 0; j < v[i].size(); j++)
-    {
-        v[i][j] = temp[counter++];
+class NoOfRotation{
+    public int fun(int arr[]){
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            int next=(mid+1)%arr.length;
+            int previous=(mid+arr.length-1)%arr.length;
+            if(arr[mid]<=arr[next] && arr[mid]<=arr[previous]){
+                return mid;
+            }
+            else {
+                if(arr[mid]>=arr[start] && arr[mid]<=arr[end]){ return start; }
+                else if(arr[mid]>=arr[start]){start=mid+1;}
+                else if(arr[mid]<=arr[end]){end=mid-1;}
+            }
+        }
+        return -1;
+//        System.out.println("No of rotation is : "+(arr.length-returned value)%arr.length);
+//        no of rotation=(arr.length-returned value)%arr.length;
     }
-}
-return v;
+  }
