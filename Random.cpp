229e3e1a6517033f68a321b9c282d1b062788dcd
Harsh <harshsharma102000@gmail.com>
2021-09-15 16:11:13 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,22 +1,85 @@
-class NoOfRotation{
-    public int fun(int arr[]){
-        int start=0;
-        int end=arr.length-1;
-        while (start<=end){
-            int mid=start+(end-start)/2;
-            int next=(mid+1)%arr.length;
-            int previous=(mid+arr.length-1)%arr.length;
-            if(arr[mid]<=arr[next] && arr[mid]<=arr[previous]){
-                return mid;
-            }
-            else {
-                if(arr[mid]>=arr[start] && arr[mid]<=arr[end]){ return start; }
-                else if(arr[mid]>=arr[start]){start=mid+1;}
-                else if(arr[mid]<=arr[end]){end=mid-1;}
+#include <bits/stdc++.h>
+
+#define int long long int
+
+using namespace std;
+
+int32_t main()
+{
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n, q;
+
+        cin >> n >> q;
+
+        vector<pair<int, int>> v;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            int a, b;
+
+            cin >> a >> b;
+
+            v.push_back({a, b});
+        }
+
+        sort(v.begin(), v.end());
+
+        int idx = 0;
+
+        for (int i = 1; i < v.size(); i++)
+        {
+
+            if (v[idx].second >= v[i].first)
+
+                v[idx].second = max(v[idx].second, v[i].second);
+
+            else
+            {
+
+                idx++;
+
+                v[idx] = v[i];
             }
         }
-        return -1;
-//        System.out.println("No of rotation is : "+(arr.length-returned value)%arr.length);
-//        no of rotation=(arr.length-returned value)%arr.length;
+
+        while (q--)
+        {
+
+            int k;
+
+            cin >> k;
+
+            int ans = -1;
+
+            for (int i = 0; i <= idx; i++)
+            {
+
+                if ((v[i].second - v[i].first + 1) >= k)
+                {
+
+                    ans = v[i].first + k - 1;
+
+                    break;
+                }
+
+                else
+                {
+
+                    k = k - (v[i].second - v[i].first + 1);
+                }
+            }
+
+            cout << ans << endl;
+        }
     }
-  }
+
+    return 0;
+}
