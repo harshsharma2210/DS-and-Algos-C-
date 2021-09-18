db5e64b636843d1ec837486d041c85247c36efd9
Harsh <harshsharma102000@gmail.com>
2021-09-18 11:25:59 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,9 +1,35 @@
-int search(int arr[], int n, int x, int k)
+long long int dp[505];
+
+class Solution
 {
-    // Complete the function
-    for (int i = 0; i < n; = "" i++)
-        = "" if (arr[i] = "=" x) = "" return = "" i;
-    = "" return = "" - 1;
-    = ""
-}
-= "" ☺☺= "" >
+public:
+    long long int solver(int ind, int k, vector<int> &arr, int pre)
+    {
+        int n = arr.size();
+        if (ind == n)
+            return (-1 * pre);
+        if (dp[ind] != -1)
+            return dp[ind];
+        int prev = 0;
+        long long int ans = INT_MAX;
+        for (int i = ind; i < n; i++)
+        {
+            prev += arr[i];
+            if (prev > k)
+                break;
+            int x = ((k - prev) * (k - prev));
+            long long int temp = x + solver(i + 1, k, arr, x);
+            ans = min(ans, temp);
+            prev += 1;
+        }
+
+        return dp[ind] = ans;
+    }
+
+    int solveWordWrap(vector<int> &nums, int k)
+    {
+
+        memset(dp, -1, sizeof(dp));
+        return solver(0, k, nums, 0);
+    }
+};
