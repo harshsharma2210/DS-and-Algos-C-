174fa8597c9f47b6625fc7b894cca2b5d59deefb
Harsh <harshsharma102000@gmail.com>
2021-11-21 13:17:41 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,48 +1,46 @@
-void alternatingSplitList(struct Node *head)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
 {
-    Node *aTail;
-    Node *bTail;
-
-    int pos = 1;
-
-    Node *curr = head;
-    while (curr != NULL)
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
     {
-        if (pos % 2 == 1)
+        bool flag = true;
+        dequeue<TreeNode *> q;
+        q.push_back(root);
+        vector<vector<int>> ans;
+        while (!q.empty())
         {
-            if (a == NULL)
+            vector<int> v;
+            int size = q.size();
+            for (int i = 0; i < size; i++)
             {
-                a = aTail = curr;
-            }
-            else
-            {
-                aTail->next = curr;
-                aTail = curr;
-            }
-        }
-        else
-        {
-            if (b == NULL)
-            {
-                b = bTail = curr;
-            }
-            else
-            {
-                bTail->next = curr;
-                bTail = curr;
-            }
-        }
 
-        pos++;
-        curr = curr->next;
+                if (flag)
+                {
+                    auto a = q.back();
+                    if (a->left)
+                    {
+                        a.push_front(a->)
+                    }
+                    if (a->right)
+                    {
+                    }
+                }
+                else
+                {
+                }
+            }
+            flag = !flag;
+        }
     }
-
-    if (a != NULL)
-    {
-        aTail->next = NULL;
-    }
-    if (b != NULL)
-    {
-        bTail->next = NULL;
-    }
-}
+};
