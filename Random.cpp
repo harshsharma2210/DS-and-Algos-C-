7b0313d8dac02157c5b4b87103f3f241743b3180
Harsh <harshsharma102000@gmail.com>
2021-12-28 15:21:47 +0530

    New

--- Random.cpp
+++ Random.cpp
@@ -1,10 +1,45 @@
-int main()
+Node *deleteNode(Node *root, int X)
 {
-    int arr[]={1,-5,2,5,-1,2,-5};
-    int i=0,j=7;
-    while(i<j)
+    if (root == NULL)
+        return NULL;
+    else if (X < root->data)
     {
-        if(arr[i]<)
+        root->left = deleteNode(root->left, X);
+        return root;
     }
+    else if (root->data == X)
+    {
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node *temp f= root->left;
+            delete root;
+            return temp;
+        }
+        if (root->left == NULL && root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
 
+        Node *rep = root->right;
+        while (rep->left != NULL)
+        {
+            rep = rep->left;
+        }
+        root->data = rep->data;
+        root->right = deleteNode(root->right, rep->data);
+        return root;
+    }
+    else
+    {
+
+        root->right = deleteNode(root->right, X);
+        return root;
+    }
 }
